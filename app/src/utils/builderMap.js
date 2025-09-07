ac377c9ae2562767aa2616a86d76ccf8a02058d3
paulovictor001 <paulo20victor06@gmail.com>
2025-09-07 17:47:45 -0300

    fix:bug corrigido

--- app/src/utils/builderMap.js
+++ app/src/utils/builderMap.js
@@ -1,10 +1,21 @@
-export default function builderMap(coordinates){
-    const map = L.map('map').setView([coordinates.lat, coordinates.log], 19);
-            
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
-        maxZoom: 20,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map)
+let map;
+let maker;
 
-    var marker = L.marker([coordinates.lat, coordinates.log]).addTo(map);
+export default function builderMap(coordinates){
+    console.log(map)
+    if (!map){
+        map = L.map('map').setView([coordinates.lat, coordinates.log], 19);
+                
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
+            maxZoom: 20,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map)
+    
+        maker = L.marker([coordinates.lat, coordinates.log]).addTo(map);
+        console.log("criou")
+    }
+    else{
+        map.setView([coordinates.lat,coordinates.log], 19)
+        maker.setLatLng([coordinates.lat, coordinates.log])
+    }
 }
